06e4537ae501b6f9dad2742c41a655857aa654cf
Aline Mattos <alinesmattos@hotmail.com>
2021-08-26 22:22:15 -0300

    Add date to 'books'

--- api/models.py
+++ api/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from datetime import now
 
 class Books(models.Model):
     name = models.CharField(max_length=256)
     number_of_pages = models.IntegerField()
     author = models.CharField(max_length=50)
+    read_date = models.DateTimeField()
     
